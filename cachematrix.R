b3cb9f6248a27236f1b7f0d1917726ae276b75c2
Juan Luis Ventura <JuanLuisVentura@users.noreply.github.com>
2015-05-22 14:36:18 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    # calculate the inverted matrix
     my_matrix <<- solve(x)
+    # return the inverted matrix
     my_matrix
 }
 
@@ -13,10 +15,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    
+    # check is the inverse is already calculated
     inverse_matrix <- my_matrix
     if (!is.null(my_natrix)) {
+        # the inverse was previously calculated
         return(inverse_matrix)
     }
+    # the inverse wasn't previously calculated, so we solve it...
     inverse_matrix <- makeCacheMatrix(x, ...)
+    # ... and return the value
     inverse_matrix
 }
