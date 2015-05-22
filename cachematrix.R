0ff8065df32811fb196e2367275c2775c5aad2cf
Juan Luis Ventura <JuanLuisVentura@users.noreply.github.com>
2015-05-22 14:32:25 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    my_matrix <<- solve(x)
+    my_matrix
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse_matrix <- my_matrix
+    if (!is.null(my_natrix)) {
+        return(inverse_matrix)
+    }
+    inverse_matrix <- makeCacheMatrix(x, ...)
+    inverse_matrix
 }
